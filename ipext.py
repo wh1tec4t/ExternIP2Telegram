51b376c0db2c297fcab073deca11742e3167fb84
vitry <106933524+wh1tec4t@users.noreply.github.com>
2023-11-04 13:23:33 +0100

    Add files via upload

--- ipext.py
+++ ipext.py
@@ -3,11 +3,12 @@ import requests
 
 def SendSMSTelegram(sms):
 
-	BOTTOKEN = 'foo'
-	BOTCHATID = 'bar'
+	BOTTOKEN = 'xxxx'
+	BOTCHATID = 'xxxx'
 
 	send_text = 'https://api.telegram.org/bot' + BOTTOKEN + '/sendMessage?chat_id=' + BOTCHATID + '&parse_mode=Markdown&text=' + sms
 	response = requests.get(send_text)
+
 	return response.json()
 
 def GetIRCIp():
@@ -17,9 +18,9 @@ def GetIRCIp():
 
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((IRCHOST, IRCPORT))
-		s.send(b'NICK Calambrazos\r\nUSER Calambrazos 8 *  :.\r\n')
+		s.send(b'NICK SlynetworkBot\r\nUSER SlynetworkBot 8 *  :.\r\n')
 		data = s.recv(1024)
-		print('Received', repr(data))
+		#print('Received', repr(data))
 		ip = 0
 		while (ip == 0):
 			op = data.split()[0]
@@ -27,14 +28,16 @@ def GetIRCIp():
 				pingid=data.split()[1]
 				s.send(b'PONG %b\r\n' % pingid)
 				data = s.recv(1024)
-				print('Received', repr(data))
+				#print('Received', repr(data))
 			elif (op == b'ERROR'):
 				ip = data.split()[3]
-				print('Received', repr(data))
-				SendSMSTelegram(str(ip))
+				#print('Received', repr(data))
 			else:
 				data = s.recv(1024)
-				print('Received', repr(data))
+				#print('Received', repr(data))
+		return ip
 
 if __name__ == '__main__':
-	GetIRCIp()
+
+	while True:
+		SendSMSTelegram(str(GetIRCIp()))
